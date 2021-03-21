b5811996cb8118610dd3932801c6fac5234d3e55
robatipoor <mahdi.robatipoor@gmail.com>
2021-03-21 21:21:17 +0330

    done traslate module

--- src/translate/mod.rs
+++ src/translate/mod.rs
@@ -1,17 +1,83 @@
-use reqwest::header::USER_AGENT;
-use strum::ToString;
-
 use lazy_static::lazy_static;
 use regex::Regex;
+use reqwest::header::USER_AGENT;
+use std::str::FromStr;
+use strum::ToString;
 
 lazy_static! {
-    static ref PUNCH: Regex = Regex::new(r"[[:punc:]]").unwrap();
+    static ref DOUBLE_QUOT: Regex = Regex::new(r#""(.*?)""#).unwrap();
 }
 
-#[derive(Debug, ToString)]
+/// Languages that can used for input and output of the [`translate`] function.
+#[derive(Debug, Clone, PartialEq, Copy, Hash, ToString)]
 pub enum Language {
-    // TODO
-    fa,
+    English,
+    Farsi,
+    Arabic,
+    Chinese,
+    French,
+    German,
+    Italian,
+    Japanese,
+    Portuguese,
+    Russian,
+    Spanish,
+}
+
+impl Language {
+    /// Return the language with the language code name. (ex. "ar", "de")
+    pub fn as_code(&self) -> &'static str {
+        match self {
+            Language::English => "en",
+            Language::Farsi => "fa",
+            Language::Arabic => "ar",
+            Language::Chinese => "zh",
+            Language::French => "fr",
+            Language::German => "de",
+            Language::Italian => "it",
+            Language::Japanese => "ja",
+            Language::Portuguese => "pt",
+            Language::Russian => "ru",
+            Language::Spanish => "es",
+        }
+    }
+
+    /// Create a Language from &str like "en" or "French". Case Doesn't matter.
+    pub fn from(s: &str) -> crate::Result<Self> {
+        return Self::from_str(s).map_err(|e| e.into());
+    }
+}
+
+impl FromStr for Language {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_string().to_lowercase().as_str() {
+            "en" => Ok(Language::English),
+            "fa" => Ok(Language::Farsi),
+            "ar" => Ok(Language::Arabic),
+            "zh" => Ok(Language::Chinese),
+            "fr" => Ok(Language::French),
+            "de" => Ok(Language::German),
+            "it" => Ok(Language::Italian),
+            "pt" => Ok(Language::Portuguese),
+            "ru" => Ok(Language::Russian),
+            "es" => Ok(Language::Spanish),
+            "ja" => Ok(Language::Japanese),
+            "english" => Ok(Language::English),
+            "farsi" => Ok(Language::Farsi),
+            "arabic" => Ok(Language::Arabic),
+            "chinese" => Ok(Language::Chinese),
+            "french" => Ok(Language::French),
+            "german" => Ok(Language::German),
+            "italian" => Ok(Language::Italian),
+            "portuguese" => Ok(Language::Portuguese),
+            "russian" => Ok(Language::Russian),
+            "spanish" => Ok(Language::Spanish),
+            "japanese" => Ok(Language::Japanese),
+            &_ => Err("input invalid!"),
+        }
+    }
 }
 
 pub trait Translate: AsRef<str> {
@@ -20,10 +86,11 @@ pub trait Translate: AsRef<str> {
     const USER_AGENT_VALUE:&'static str = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
 
     fn translate(&self, target: Language) -> crate::Result<String> {
+
         let url = format!(
             "{url}&tl={target}&hl={target}&dt=t&text={text}",
             url = Self::BASE_URL,
-            target = target.to_string(),
+            target = target.as_code(),
             text = &self.as_ref(),
         );
         reqwest::blocking::Client::new()
@@ -33,13 +100,13 @@ pub trait Translate: AsRef<str> {
             .text()
             .map_err(|e| e.to_string())
             .and_then(|s| {
-                s.replace("[", "")
-                    .replace("]", "")
-                    .split(",")
-                    //TODO FIX BUG 
-                    .next()
-                    .map(|n| n.to_string())
-                    .ok_or_else(|| "Does Not Exis".into())
+                if let Some(c) = DOUBLE_QUOT.captures_iter(&s).next() {
+                    let content = c[0].trim();
+                    let len = content.len();
+                    Ok(content[1..len - 1].to_string())
+                } else {
+                    Err("Does Not Exist".to_string())
+                }
             })
             .map_err(|e| e.into())
     }
@@ -52,11 +119,11 @@ impl Translate for str {}
 #[cfg(test)]
 mod translate {
     use super::{Language, Translate};
-
+    
     #[test]
     fn translate() {
-        let salam = "Hello".translate(Language::fa).unwrap();
-        println!("{}", salam);
-        assert_eq!("سلام", salam);
+        assert_eq!(
+            "سلام دنیا",r#"Hello, World"#.translate(Language::Farsi).unwrap()
+        );
     }
 }
