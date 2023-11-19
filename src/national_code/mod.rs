8146e1ef1db270293ff242c1ba0f3026abb5cb61
YouKnow <saeid003424@gmail.com>
2023-11-20 03:16:54 +0330

    improved `NationalCode` and disabled `translate` module by default.

--- src/national_code/mod.rs
+++ src/national_code/mod.rs
@@ -4,21 +4,15 @@ pub trait NationalCode: AsRef<str> {
     /// Takes a string and check if it's a valid Iranian national code or not.
     fn is_valid_national_code(&self) -> bool {
         let code = self.as_ref();
-        if !(code.len() == 10 && code.chars().all(|c| c.is_ascii_digit())) {
-            return false;
-        }
-        if let Ok(num) = code[3..].parse::<u32>() {
-            if num == 0 {
-                return false;
-            }
-        }
-        let last_index = code[9..].parse::<u32>().unwrap();
-        let mut sum: u32 = 0;
-        for i in 0..9 {
-            sum += code[i..i + 1].parse::<u32>().unwrap() * (10 - i) as u32;
-        }
-        sum %= 11;
-        (sum < 2 && last_index == sum) || (sum >= 2 && last_index == 11 - sum)
+        if code.len() != 10 { return false; }
+
+        let digits: Vec<u32> = code.chars().map_while(|c| c.to_digit(10)).collect();
+        if digits.len() != 10 { return false; }
+        
+        let last = digits[9];
+        let sum = (0..9).map(|x| digits[x] * (10 - x) as u32).sum::<u32>() % 11;
+
+        (sum < 2 && last == sum) || (last + sum == 11)
     }
 }
 
